e94c7a339af892a67e77b7d3a5ab7d0fbb2feafe
progman <gnuplanet@gmail.com>
2023-06-19 19:33:40 +0600

    update

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DOCKER_IMAGE="gkrellflynn_builder_image";
+DOCKER_CONTAINER="gkrellflynn_builder";
+TARGET_FILE="gkrellflynn.so";
+
 which docker &> /dev/null < /dev/null;
 if [ "${?}" != "0" ];
 then
@@ -7,41 +11,41 @@ then
 	exit 1;
 fi
 
-docker build --no-cache --tag gkrellflynn_builder_image ./;
+docker build --no-cache --tag "${DOCKER_IMAGE}" ./;
 if [ "${?}" != "0" ];
 then
 	echo "ERROR: docker build";
 	exit 1;
 fi
 
-docker create --name gkrellflynn_builder gkrellflynn_builder_image;
+docker create --name "${DOCKER_CONTAINER}" "${DOCKER_IMAGE}";
 if [ "${?}" != "0" ];
 then
 	echo "ERROR: docker create";
 	exit 1;
 fi
 
-docker cp gkrellflynn_builder:/app/gkrellflynn.so ./;
+docker cp "${DOCKER_CONTAINER}":/app/"${TARGET_FILE}" ./;
 if [ "${?}" != "0" ];
 then
 	echo "ERROR: docker cp";
 	exit 1;
 fi
 
-docker rm -f gkrellflynn_builder;
+docker rm -f "${DOCKER_CONTAINER}";
 if [ "${?}" != "0" ];
 then
 	echo "ERROR: docker rm";
 	exit 1;
 fi
 
-docker rmi gkrellflynn_builder_image;
+docker rmi "${DOCKER_IMAGE}";
 if [ "${?}" != "0" ];
 then
 	echo "ERROR: docker rmi";
 	exit 1;
 fi
 
-echo "gkrellflynn.so is ready";
+echo "Done";
 
 exit 0;
